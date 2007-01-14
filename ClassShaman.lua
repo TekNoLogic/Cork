26a57cb945fd6a46fb487453af6f2b97c6f25977
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-01-14 00:50:15 +0000

    FuBar_CorkFu - Shaman crap from Vythus
    git-svn-id: http://dev.wowace.com/wowace/trunk/FuBar_CorkFu@25036 48e1f902-e210-0410-a839-a0138e9d5bd5

--- ClassShaman.lua
+++ ClassShaman.lua
@@ -7,6 +7,22 @@ local core, i = FuBar_CorkFu
 local buffs = core:GetTemplate("Buffs")
 
 
+i = core:NewModule(B["Earth Shield"], buffs)
+i.target = "Friendly"
+i.spell = B["Earth Shield"]
+
+
 i = core:NewModule(B["Lightning Shield"], buffs)
 i.spell = B["Lightning Shield"]
 i.target = "Self"
+
+
+i = core:NewModule(B["Water Breathing"], buffs)
+i.target = "Friendly"
+i.spell = B["Water Breathing"]
+
+
+i = core:NewModule(B["Water Walking"], buffs)
+i.target = "Friendly"
+i.spell = B["Water Walking"]
+
