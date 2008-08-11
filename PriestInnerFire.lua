b2f8ab18d09503488bb28e861e12a59542170aad
Tekkub <tekkub@gmail.com>
2008-08-11 01:22:30 -0600

    Strip down IFire module

--- PriestInnerFire.lua
+++ PriestInnerFire.lua
@@ -4,17 +4,12 @@ if c ~= "PRIEST" then return end
 
 
 local spellname, _, icon = GetSpellInfo(588)
-local iconline = IconLine(icon, UnitName("player"))
-local SpellCastableOnUnit, IconLine = Cork.SpellCastableOnUnit, Cork.IconLine
-local thresh = 2
-
-
-local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
-local dataobj = ldb:NewDataObject("Cork_InnerFire", {type = "cork"})
-
+local iconline = Cork.IconLine(icon, UnitName("player"))
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork_InnerFire", {type = "cork"})
 
 local function Test(unit) if not UnitAura("player", spellname) then return iconline end end
-ae.RegisterEvent("Cork_InnerFire", "UNIT_AURA", function(event, unit) if unit == "player" then dataobj.player = Test() end end)
+
+LibStub("AceEvent-3.0").RegisterEvent("Cork_InnerFire", "UNIT_AURA", function(event, unit) if unit == "player" then dataobj.player = Test() end end)
 dataobj.player = Test()
 
 
