3f5c1e108cab5d26e154f09ba4ff7c0ba9f4544f
Tekkub <tekkub@gmail.com>
2008-08-26 02:00:52 -0600

    Hunter modules

---
+++ classes/Hunter.lua
@@ -0,0 +1,13 @@
+
+local _, c = UnitClass("player")
+if c ~= "HUNTER" then return end
+
+
+-- Aspects
+Cork:GenerateAdvancedSelfBuffer("Aspects", {13165, 13161, 13163, 5118, 13159, 20043, 34074})
+
+
+-- Trueshot Aura
+local spellname, _, icon = GetSpellInfo(19506)
+Cork:GenerateSelfBuffer(spellname, icon)
+
