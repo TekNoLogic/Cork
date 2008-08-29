b8476490efceab9aa1c59deaab8f66b69d9c8ac2
Tekkub <tekkub@gmail.com>
2008-08-29 01:32:10 -0600

    Add macro generator, rejigger base config panel

--- Config.lua
+++ Config.lua
@@ -34,27 +34,9 @@ frame:SetScript("OnShow", function()
 		Cork.Update()
 	end)
 
-	local castonpets, groupthresh, groupthreshtext
-	if Cork.hasgroupspell then
-		castonpets = tekcheck.new(frame, nil, "Cast on group pets", "TOP", showanchor, "TOP")
-		castonpets:SetPoint("LEFT", frame, "CENTER", GAP/2, 0)
-		castonpets.tiptext = "Pets need buffs too!  When disabled you can still cast on a pet by targetting it directly."
-		castonpets:SetScript("OnClick", function(self)
-			checksound(self)
-			Cork.dbpc.castonpets = not Cork.dbpc.castonpets
-			for name,dataobj in pairs(Cork.corks) do dataobj:Scan() end
-		end)
 
-		groupthresh, groupthreshtext = LibStub("tekKonfig-Slider").new(frame, "Group Threshold: ".. Cork.dbpc.multithreshold, 1, 6, "TOPLEFT", castonpets, "BOTTOMLEFT", GAP*2, -GAP)
-		groupthresh.tiptext = "Minimum number of needy players in a group required to cast multi-target spells.  Setting this to six will disable the automatic use of group spells."
-		groupthresh:SetValueStep(1)
-		groupthresh:SetScript("OnValueChanged", function(self, newvalue)
-			Cork.dbpc.multithreshold = newvalue
-			groupthreshtext:SetText("Group Threshold: ".. newvalue)
-		end)
-	end
-
-	local tooltiplimit, tooltiplimittext = LibStub("tekKonfig-Slider").new(frame, "Tooltip Limit: " .. Cork.dbpc.tooltiplimit, 0, 40, "TOPLEFT", showunit, "BOTTOMLEFT", GAP*2, -GAP)
+	local tooltiplimit, tooltiplimittext, ttlcontainer = LibStub("tekKonfig-Slider").new(frame, "Tooltip Limit: " .. Cork.dbpc.tooltiplimit, 0, 40, "TOP", showanchor, "TOP")
+	ttlcontainer:SetPoint("LEFT", frame, "CENTER", GAP*5/2, 0)
 	tooltiplimit.tiptext = "The number of units to show in the Cork tooltip."
 	tooltiplimit:SetValueStep(1)
 	tooltiplimit:SetValue(Cork.dbpc.tooltiplimit)
@@ -63,9 +45,39 @@ frame:SetScript("OnShow", function()
 		tooltiplimittext:SetText("Tooltip Limit: " .. newvalue)
 	end)
 
-	local group = LibStub("tekKonfig-Group").new(frame, "Modules", "TOP", tooltiplimit, "BOTTOM", 0, -27)
+
+	local castonpets, groupthresh, groupthreshtext, groupthreshcont
+	if Cork.hasgroupspell then
+		groupthresh, groupthreshtext, groupthreshcont = LibStub("tekKonfig-Slider").new(frame, "Group Threshold: ".. Cork.dbpc.multithreshold, 1, 6, "TOPLEFT", ttlcontainer, "BOTTOMLEFT") --, GAP*2, -GAP)
+		groupthresh.tiptext = "Minimum number of needy players in a group required to cast multi-target spells.  Setting this to six will disable the automatic use of group spells."
+		groupthresh:SetValueStep(1)
+		groupthresh:SetScript("OnValueChanged", function(self, newvalue)
+			Cork.dbpc.multithreshold = newvalue
+			groupthreshtext:SetText("Group Threshold: ".. newvalue)
+		end)
+
+--~ 		castonpets = tekcheck.new(frame, nil, "Cast on group pets", "TOPLEFT", groupthreshcont, "BOTTOMLEFT", -GAP*2, 0)
+		castonpets = tekcheck.new(frame, nil, "Cast on group pets", "TOPLEFT", showunit, "BOTTOMLEFT", 0, -GAP)
+		castonpets.tiptext = "Pets need buffs too!  When disabled you can still cast on a pet by targetting it directly."
+		castonpets:SetScript("OnClick", function(self)
+			checksound(self)
+			Cork.dbpc.castonpets = not Cork.dbpc.castonpets
+			for name,dataobj in pairs(Cork.corks) do dataobj:Scan() end
+		end)
+	end
+
+
+	local group = LibStub("tekKonfig-Group").new(frame, "Modules", "TOP", castonpets or showunit, "BOTTOM", 0, -27)
 	group:SetPoint("LEFT", EDGEGAP, 0)
 	group:SetPoint("BOTTOMRIGHT", -EDGEGAP, EDGEGAP)
+
+
+	local macrobutt = LibStub("tekKonfig-Button").new_small(frame, "BOTTOMRIGHT", group, "TOPRIGHT")
+	macrobutt:SetWidth(60) macrobutt:SetHeight(18)
+	macrobutt.tiptext = "Click to generate a macro, or pick it up if already generated."
+	macrobutt:SetText("Macro")
+	macrobutt:SetScript("OnClick", Cork.GenerateMacro)
+
 
 	local rows, corknames, anchor = {}, {}
 	for name in pairs(Cork.corks) do table.insert(corknames, (name:gsub("Cork ", ""))) end
---
+++ MacroGenerator.lua
@@ -0,0 +1,35 @@
+
+local MAX_ACCOUNT_MACROS, MAX_CHARACTER_MACROS = IS_WRATH_BUILD and 36 or 18, 18
+
+local function GetMacroID()
+	for i=(MAX_ACCOUNT_MACROS+1),(MAX_ACCOUNT_MACROS+MAX_CHARACTER_MACROS) do if GetMacroInfo(i) == "Cork" then return i end end
+end
+
+function Cork.GenerateMacro()
+	if InCombatLockdown() then return end
+
+	local id = GetMacroID()
+	if id then PickupMacro(id)
+	elseif select(2, GetNumMacros()) < MAX_CHARACTER_MACROS then
+		local body, ic, ooc
+		local _, c = UnitClass("player")
+		if c == "DEATHKNIGHT" then ooc = GetSpellInfo(3714)
+		elseif c == "DRUID" then ic, ooc = GetSpellInfo(22812), GetSpellInfo(GetSpellInfo(21562)) or GetSpellInfo(1126)
+		elseif c == "HUNTER" then ooc = GetSpellInfo(13165)
+		elseif c == "MAGE" then ic, ooc = GetSpellInfo(168), GetSpellInfo(GetSpellInfo(23028)) or GetSpellInfo(1459)
+		elseif c == "PALADIN" then ic, ooc = GetSpellInfo(21084), GetSpellInfo(GetSpellInfo(25782)) or GetSpellInfo(19740)
+		elseif c == "PRIEST" then ic, ooc = GetSpellInfo(588), GetSpellInfo(GetSpellInfo(21849)) or GetSpellInfo(1243)
+		elseif c == "SHAMAN" then ooc = GetSpellInfo(324)
+		elseif c == "WARLOCK" then ooc = GetSpellInfo(687)
+		elseif c == "WARRIOR" then ooc = GetSpellInfo(6673) end
+		if ic and ooc then
+			body = "#showtooltip [combat] "..ic.."; "..ooc.."\n/cast [combat] "..ic.."\n/stopmacro [combat]\n/click CorkFrame"
+		elseif ooc then
+			body = "#showtooltip "..ooc.."\n/click CorkFrame"
+		else
+			body = "/click CorkFrame"
+		end
+		local id = CreateMacro("Cork", 1, body, true)
+		PickupMacro(id)
+	end
+end
