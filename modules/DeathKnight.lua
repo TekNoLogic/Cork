3e2a5102f8da404c16520e6adeb52118a9859b98
Tekkub <tekkub@gmail.com>
2008-08-29 01:32:24 -0600

    Add Path of Frost to DKs

--- modules/DeathKnight.lua
+++ modules/DeathKnight.lua
@@ -6,3 +6,8 @@ if c ~= "DEATHKNIGHT" then return end
 -- Bone Shield
 local spellname, _, icon = GetSpellInfo(49222)
 Cork:GenerateSelfBuffer(spellname, icon)
+
+
+-- Path of Frost
+local spellname, _, icon = GetSpellInfo(3714)
+Cork:GenerateSelfBuffer(spellname, icon)
