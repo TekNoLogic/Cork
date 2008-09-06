90e538e8d6d7bfe328df76691bb61b49c85a66ea
Tekkub <tekkub@gmail.com>
2008-09-06 13:51:41 -0600

    OCD, why do you torment me so?

--- MacroGenerator.lua
+++ MacroGenerator.lua
@@ -14,14 +14,14 @@ function Cork.GenerateMacro()
 		local body, ic, ooc
 		local _, c = UnitClass("player")
 		if c == "DEATHKNIGHT" then ooc = GetSpellInfo(3714)
-		elseif c == "DRUID" then ic, ooc = GetSpellInfo(22812), GetSpellInfo(GetSpellInfo(21562)) or GetSpellInfo(1126)
-		elseif c == "HUNTER" then ooc = GetSpellInfo(13165)
-		elseif c == "MAGE" then ic, ooc = GetSpellInfo(168), GetSpellInfo(GetSpellInfo(23028)) or GetSpellInfo(1459)
-		elseif c == "PALADIN" then ic, ooc = GetSpellInfo(21084), GetSpellInfo(GetSpellInfo(25782)) or GetSpellInfo(19740)
-		elseif c == "PRIEST" then ic, ooc = GetSpellInfo(588), GetSpellInfo(GetSpellInfo(21849)) or GetSpellInfo(1243)
-		elseif c == "SHAMAN" then ooc = GetSpellInfo(324)
+		elseif c == "HUNTER"  then ooc = GetSpellInfo(13165)
+		elseif c == "SHAMAN"  then ooc = GetSpellInfo(324)
 		elseif c == "WARLOCK" then ooc = GetSpellInfo(687)
-		elseif c == "WARRIOR" then ooc = GetSpellInfo(6673) end
+		elseif c == "WARRIOR" then ooc = GetSpellInfo(6673)
+		elseif c == "DRUID"   then ic, ooc = GetSpellInfo(22812), GetSpellInfo(GetSpellInfo(21562)) or GetSpellInfo(1126)
+		elseif c == "MAGE"    then ic, ooc = GetSpellInfo(168),   GetSpellInfo(GetSpellInfo(23028)) or GetSpellInfo(1459)
+		elseif c == "PALADIN" then ic, ooc = GetSpellInfo(21084), GetSpellInfo(GetSpellInfo(25782)) or GetSpellInfo(19740)
+		elseif c == "PRIEST"  then ic, ooc = GetSpellInfo(588),   GetSpellInfo(GetSpellInfo(21849)) or GetSpellInfo(1243) end
 		if ic and ooc then
 			body = "#showtooltip [combat] "..ic.."; "..ooc.."\n/cast [combat] "..ic.."\n/stopmacro [combat]\n/click CorkFrame"
 		elseif ooc then
