6f5f39930cc3342df5035384d74bdcf53a2041d1
Tekkub <tekkub@gmail.com>
2008-10-17 15:24:08 -0600

    Remove Omen of Clarity, is now passive

--- modules/Druid.lua
+++ modules/Druid.lua
@@ -8,11 +8,6 @@ local multispell, spellname, _, icon = GetSpellInfo(21849), GetSpellInfo(1126)
 Cork:GenerateRaidBuffer(spellname, multispell, icon)
 
 
--- Omen of Clarity
-local spellname, _, icon = GetSpellInfo(16864)
-Cork:GenerateSelfBuffer(spellname, icon)
-
-
 -- Shapeshifts
 local bear = GetSpellInfo(GetSpellInfo(5487)) and 5487 or 9634
 Cork:GenerateAdvancedSelfBuffer("Fursuit", {bear, 768, 24858, 33891})
