9dd06a0829a034df8a58640b525b7687d086673c
Tekkub <tekkub@gmail.com>
2008-10-31 20:16:16 -0600

    Make good use of 'self'

--- modules/HunterViper.lua
+++ modules/HunterViper.lua
@@ -1,6 +1,8 @@
 
 local _, token = UnitClass("player")
 if token ~= "HUNTER" then return end
+
+
 local spellname, _, icon = GetSpellInfo(34074)
 
 local Cork = Cork
@@ -11,11 +13,13 @@ local iconline = Cork.IconLine(icon, UnitName("player"), token)
 
 local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork "..spellname, {type = "cork"})
 
+
 function dataobj:Init()
 	Cork.defaultspc["Aspect of the Viper-enabled"] = GetSpellInfo(spellname) ~= nil
 	Cork.defaultspc["Aspect of the Viper-low threshold"] = 0.2
 	Cork.defaultspc["Aspect of the Viper-high threshold"] = 0.6
 end
+
 
 local function Test()
 	if Cork.dbpc["Aspect of the Viper-enabled"] then
@@ -28,24 +32,27 @@ local function Test()
 	end
 end
 
+
 function dataobj:Scan()
 	self.player = Test()
 end
+
 
 local function EventUpdate(event, unit)
 	if unit == "player" then
 		dataobj:Scan()
 	end
 end
-
 LibStub("AceEvent-3.0").RegisterEvent("Cork Aspect of the Viper", "UNIT_AURA", EventUpdate)
 LibStub("AceEvent-3.0").RegisterEvent("Cork Aspect of the Viper", "UNIT_MANA", EventUpdate)
+
 
 function dataobj:CorkIt(frame)
 	if self.player then
 		return frame:SetManyAttributes("type1", "spell", "spell", spellname, "unit", "player")
 	end
 end
+
 
 ----------------------
 --      Config      --
@@ -57,8 +64,7 @@ frame:SetHeight(1)
 dataobj.configframe = frame
 frame:Hide()
 
-frame:SetScript("OnShow", function()
-
+frame:SetScript("OnShow", function(self)
 	local function createSlider(parent, setting, tiptext, ...)
 		local slider = LibStub("tekKonfig-Slider").newbare(parent, ...)
 		slider:SetWidth(72)
@@ -79,14 +85,14 @@ frame:SetScript("OnShow", function()
 		return slider
 	end
 
-	local sliderHigh = createSlider(frame, 'high threshold', "High mana threshold. Cork will remember you to remove Aspect of the Viper when your mana is above this threshold.", "RIGHT")
-	local sliderLow = createSlider(frame, 'low threshold', "Low mana threshold. Cork will remember you to switch to Aspect of the Viper when your mana is under this threshold.", "RIGHT", sliderHigh, "LEFT", -40, 0)
+	local sliderHigh = createSlider(self, 'high threshold', "High mana threshold. Cork will remember you to remove Aspect of the Viper when your mana is above this threshold.", "RIGHT")
+	local sliderLow = createSlider(self, 'low threshold', "Low mana threshold. Cork will remember you to switch to Aspect of the Viper when your mana is under this threshold.", "RIGHT", sliderHigh, "LEFT", -30, 0)
 
-	local function Update(self)
+	local function Update()
 		sliderLow:SetValue(Cork.dbpc["Aspect of the Viper-low threshold"])
 		sliderHigh:SetValue(Cork.dbpc["Aspect of the Viper-high threshold"])
 	end
 
-	frame:SetScript("OnShow", Update)
-	Update(frame)
+	self:SetScript("OnShow", Update)
+	Update()
 end)
