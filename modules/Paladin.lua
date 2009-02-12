0336b2a1d03f29523e926fcded7e2403ac8a1506
Tekkub <tekkub@gmail.com>
2009-02-11 21:56:31 -0700

    Move pally seals to normal buffs

--- modules/Paladin.lua
+++ modules/Paladin.lua
@@ -10,3 +10,7 @@ Cork:GenerateSelfBuffer(spellname, icon)
 
 -- Auras
 Cork:GenerateAdvancedSelfBuffer("Aura", {465, 7294, 19746, 19876, 19888, 19891, 32223})
+
+-- Seals
+local isawhorde = UnitFactionGroup("player") == "Horde"
+Cork:GenerateAdvancedSelfBuffer("Seal", {21084, 20375, isawhorde and 31892 or 53720, 20166, isawhorde and 53736 or 31801, 20165, 20164})
