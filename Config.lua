30ed570543812b79c7a611413f10016400e0a58c
Tekkub <tekkub@gmail.com>
2009-03-12 00:16:56 -0600

    Clarify a tooltip just a smidge

--- Config.lua
+++ Config.lua
@@ -71,7 +71,7 @@ frame:SetScript("OnShow", function()
 	local castonpets, groupthresh, groupthreshtext, groupthreshcont
 	if Cork.hasgroupspell then
 		groupthresh, groupthreshtext, groupthreshcont = LibStub("tekKonfig-Slider").new(frame, "Group Threshold: ".. Cork.dbpc.multithreshold, 1, 6, "TOPLEFT", ttlcontainer, "BOTTOMLEFT") --, GAP*2, -GAP)
-		groupthresh.tiptext = "Minimum number of needy players in a group required to cast multi-target spells.  Setting this to six will disable the automatic use of group spells."
+		groupthresh.tiptext = "Minimum number of needy players in a group required to cast multi-target spells.  Setting this to six will disable the automatic use of group spells when in a party."
 		groupthresh:SetValueStep(1)
 		groupthresh:SetScript("OnValueChanged", function(self, newvalue)
 			Cork.dbpc.multithreshold = newvalue
