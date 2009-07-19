37a917e7284204b79630920e6c090a2063d09a94
Tekkub <tekkub@gmail.com>
2009-07-19 17:38:18 -0600

    Fix talents icon, show at all times (not just when resting)

--- modules/Talents.lua
+++ modules/Talents.lua
@@ -1,25 +1,16 @@
--- Hack repairs into talents
 
 local Cork = Cork
-local SpellCastableOnUnit, IconLine = Cork.SpellCastableOnUnit, Cork.IconLine
+local SpellCastableOnUnit = Cork.SpellCastableOnUnit
 local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
-local ICON = "Interface\\Minimap\\Tracking\\Repair"
+local IconLine = Cork.IconLine("Interface\\Icons\\Ability_Marksmanship", "Unspent talent points")
 local defaults = Cork.defaultspc
 defaults["Talents-enabled"] = true
 
 local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork Talents", {type = "cork"})
 
-local function Test()
-	if not Cork.dbpc["Talents-enabled"] or not IsResting() then return end
-
-	local points = UnitCharacterPoints("player")
-
-	if points < 1 then return end
-	return IconLine(ICON, "You have " ..  points .. " unspent Talent Points.")
-end
+local function Test() return Cork.dbpc["Talents-enabled"] and UnitCharacterPoints("player") > 0 and IconLine end
 
 function dataobj:Scan() dataobj.player = Test() end
 
 ae.RegisterEvent("Cork Talents", "CHARACTER_POINTS_CHANGED", dataobj.Scan)
-ae.RegisterEvent("Cork Talents", "PLAYER_UPDATE_RESTING", dataobj.Scan)
