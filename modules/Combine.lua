667dbf6456ccee2edca8185d30f2000705440b99
tekkub <tekkub@gmail.com>
2009-10-06 01:12:16 -0600

    Clean up combine module

--- modules/Combine.lua
+++ modules/Combine.lua
@@ -3,44 +3,35 @@ local Cork = Cork
 local IconLine = Cork.IconLine
 local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
-local MODULE = "Combine"
-local ITEMS = {
-	[37700] = 10, -- Crystallized Air
-	[37701] = 10, -- Crystallized Earth
-	[37702] = 10, -- Crystallized Fire
-	[37703] = 10, -- Crystallized Shadow
-	[37704] = 10, -- Crystallized Life
-	[37705] = 10, -- Crystallized Water
-	[33567] = 5,  -- Borean Leather Scraps
-	[34056] = 3,  -- Lesser Cosmic Essence
-}
+local ITEMS = {[37700] = 10, [37701] = 10, [37702] = 10, [37703] = 10, [37704] = 10, [37705] = 10, [33567] = 5, [34056] = 3}
 
 Cork.defaultspc[MODULE.."-enabled"] = true
 
-local dataobj = ldb:NewDataObject("Cork "..MODULE, {type = "cork"})
+local dataobj = ldb:NewDataObject("Cork Combine", {type = "cork"})
 
 function dataobj:Scan()
-	if Cork.dbpc[MODULE.."-enabled"] and not InCombatLockdown() then
-		for id,threshold in pairs(ITEMS) do
-			local count = GetItemCount(id) or 0
-			if count >= threshold then
-				local itemName, _, _, _, _, _, _, _, _, itemTexture = GetItemInfo(id) 
-				dataobj.player = IconLine(itemTexture, itemName.." ("..count..")")
-				return
-			end
+	if not Cork.dbpc["Combine-enabled"] or InCombatLockdown() then
+		dataobj.player = nil
+		return
+	end
+
+	for id,threshold in pairs(ITEMS) do
+		local count = GetItemCount(id) or 0
+		if count >= threshold then
+			local itemName, _, _, _, _, _, _, _, _, itemTexture = GetItemInfo(id)
+			dataobj.player = IconLine(itemTexture, itemName.." ("..count..")")
+			return
 		end
 	end
-	dataobj.player = nil
 end
 
-ae.RegisterEvent("Cork "..MODULE, "BAG_UPDATE", dataobj.Scan)
+ae.RegisterEvent("Cork Combine", "BAG_UPDATE", dataobj.Scan)
 
 function dataobj:CorkIt(frame)
 	if dataobj.player then
 		for id,threshold in pairs(ITEMS) do
-			if (GetItemCount(id) or 0) >= threshold then 
-				dataobj.player = nil
-				return frame:SetManyAttributes("type1", "item", "item1", "item:"..id) 
+			if (GetItemCount(id) or 0) >= threshold then
+				return frame:SetManyAttributes("type1", "item", "item1", "item:"..id)
 			end
 		end
 	end
