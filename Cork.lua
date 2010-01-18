14c90553281c71bccd35633504ba0562df8539f1
Adirelle <adirelle@tagada-team.net>
2010-01-18 14:30:20 +0100

    Do not tell me to buff disconnected or non-visible characters.

--- Cork.lua
+++ Cork.lua
@@ -250,7 +250,7 @@ end
 
 function Cork.SpellCastableOnUnit(spell, unit)
 	if Cork.keyblist[i] then return end
-	return UnitExists(unit) and UnitCanAssist("player", unit) and not UnitIsDeadOrGhost(unit) and Cork.IsSpellInRange(spell, unit)
+	return UnitExists(unit) and UnitCanAssist("player", unit) and UnitIsVisible(unit) and UnitIsConnected(unit) and not UnitIsDeadOrGhost(unit) and Cork.IsSpellInRange(spell, unit)
 end
 
 function Cork.IconLine(icon, text, token)
