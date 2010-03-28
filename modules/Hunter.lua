3d8f8dd230772b135db7cc3ef94341d1969e081c
Adirelle <adirelle@tagada-team.net>
2010-03-28 14:44:52 +0200

    Fixed Abominable Might spell id.

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -10,7 +10,7 @@ Cork:GenerateAdvancedSelfBuffer("Aspects", {13165, 13161, 13163, 5118, 13159, 20
 
 -- Trueshot Aura
 local spellname, _, icon = GetSpellInfo(19506)
-local abom = GetSpellInfo(53136) --Abominable Might
+local abom = GetSpellInfo(53138) --Abominable Might
 local rage = GetSpellInfo(30802) --Unleashed Rage
 Cork:GenerateSelfBuffer(spellname, icon, abom, rage)
 
