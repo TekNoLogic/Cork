a1839ac1e7bab9c747ad462380e02761e3a498ab
tekkub <tekkub@gmail.com>
2010-05-31 02:08:05 -0600

    Clean up new mana gem module

--- modules/MageGem.lua
+++ modules/MageGem.lua
@@ -2,43 +2,31 @@
 local _, c = UnitClass("player")
 if c ~= "MAGE" then return end
 
-local Cork = Cork
-local SpellCastableOnUnit, IconLine = Cork.SpellCastableOnUnit, Cork.IconLine
+
+local myname, Cork = ...
 local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
-local conjurespellname = GetSpellInfo(759)
-local conjurespell = GetSpellInfo(conjurespellname)
+local ITEMS = {5514, 8007, 5513, 8008, 33312}
+local spellname, _, icon = GetSpellInfo(759)
+local IconLine = Cork.IconLine(icon, spellname)
 
-local ICON, ITEMS = "Interface\\Icons\\INV_Misc_Gem_Sapphire_02", { 5514, 8007, 5513, 8008, 33312 }
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork "..spellname, {type = "cork"})
 
-Cork.defaultspc["Mana Gem-enabled"] = true
 
-local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork Mana Gem", {type = "cork"})
-
-function dataobj:Scan()
-	if not Cork.dbpc["Mana Gem-enabled"] then
-		dataobj.player = nil
-		return
-	end
-
-	local count = 0
-	for _,id in pairs(ITEMS) do
-		local charges = GetItemCount(id, nil, true)
-		if charges == 3 then
-			count = count + 1
-		end
-	end
-
-	if count == 0 then dataobj.player = IconLine(ICON, "Mana Gem")
-	else dataobj.player = nil end
+function dataobj:Init()
+	Cork.defaultspc[spellname.."-enabled"] = GetSpellInfo(spellname) ~= nil
 end
 
-ae.RegisterEvent("Cork Mana Gem", "BAG_UPDATE", dataobj.Scan)
-ae.RegisterEvent("Cork Mana Gem", "BAG_UPDATE_COOLDOWN", dataobj.Scan)
+
+function dataobj:Scan()
+	if not Cork.dbpc[spellname.."-enabled"] then dataobj.player = nil; return end
+	for _,id in pairs(ITEMS) do if GetItemCount(id) > 0 then dataobj.player = nil; return end	end
+	dataobj.player = IconLine
+end
+ae.RegisterEvent("Cork "..spellname, "BAG_UPDATE", dataobj.Scan)
+ae.RegisterEvent("Cork "..spellname, "BAG_UPDATE_COOLDOWN", dataobj.Scan)
+
 
 function dataobj:CorkIt(frame)
-	-- refresh if they just learned it
-	conjurespell = conjurespell or GetSpellInfo(conjurespellname)
-
-	if self.player then return frame:SetManyAttributes("type1", "spell", "spell", conjurespell, "unit", "player") end
+	if self.player then return frame:SetManyAttributes("type1", "spell", "spell", spellname) end
 end
