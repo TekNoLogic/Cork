f5f5662e8f6996fb17e957960112031f6aff7355
tekkub <tekkub@gmail.com>
2010-09-04 03:02:13 -0600

    New unlearned spells module, only works in Cat

---
+++ modules/Training.lua
@@ -0,0 +1,23 @@
+
+local myname, Cork = ...
+if not Cork.IHASCAT then return end
+local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
+
+local IconLine = Cork.IconLine("Interface\\Icons\\inv_misc_book_09", "Unlearned spells")
+Cork.defaultspc["Training-enabled"] = true
+
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork Training", {type = "cork", tiptext = "Warn when you have abilities or spells to learn from a trainer."})
+
+local function NeedToTrain()
+	for i=1,MAX_SPELLS do
+		local spelltype = GetSpellBookItemInfo(i, "spell")
+		if not spelltype then return end
+		if spelltype == "FUTURESPELL" and (GetSpellAvailableLevel(i, "spell") or math.huge) <= UnitLevel("player") then return true end
+	end
+end
+local function Test() return Cork.dbpc["Training-enabled"] and NeedToTrain() and IconLine end
+
+function dataobj:Scan() dataobj.player = Test() end
+
+ae.RegisterEvent("Cork Training", "SPELLS_CHANGED", dataobj.Scan)
+ae.RegisterEvent("Cork Training", "LEARNED_SPELL_IN_TAB", dataobj.Scan)
