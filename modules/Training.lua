a6ff7cc40ef0e3ae4dae93b25f67e220248d6fba
tekkub <tekkub@gmail.com>
2010-10-14 03:40:39 -0600

    Ignore azeroth flying on live, for now

--- modules/Training.lua
+++ modules/Training.lua
@@ -7,13 +7,16 @@ Cork.defaultspc["Training-enabled"] = true
 
 local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork Training", {type = "cork", tiptext = "Warn when you have abilities or spells to learn from a trainer."})
 
+local blacklist = {
+	[34091] = true, -- artisan riding (epic flying)
+	[90265] = true, -- master riding (super-epic flying)
+	[90267] = not GetGuildLevelEnabled(), -- azeroth flying, not in live but shows up in the spellbook
+}
 local function NeedToTrain()
 	for i=1,MAX_SPELLS do
 		local spelltype, spellid = GetSpellBookItemInfo(i, "spell")
-		if not spelltype then return end
-		-- 34091 is artisan riding (epic flying)
-		-- 90265 is master riding (super-epic flying)
-		if spelltype == "FUTURESPELL" and spellid ~= 34091 and spellid ~= 90265 and (GetSpellAvailableLevel(i, "spell") or math.huge) <= UnitLevel("player") then return true end
+		if not spelltype or blacklist[spellid] then return end
+		if spelltype == "FUTURESPELL" and (GetSpellAvailableLevel(i, "spell") or math.huge) <= UnitLevel("player") then return true end
 	end
 end
 local function Test() return Cork.dbpc["Training-enabled"] and NeedToTrain() and IconLine end
