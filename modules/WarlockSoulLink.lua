3d6ba4c1d87bd6485360fefd54e3f2630bcdf5f9
tekkub <tekkub@gmail.com>
2010-11-28 20:56:35 -0700

    No point in using pet names in the soul link module

--- modules/WarlockSoulLink.lua
+++ modules/WarlockSoulLink.lua
@@ -4,11 +4,11 @@ if Cork.MYCLASS ~= "WARLOCK" then return end
 
 local myname, Cork = ...
 local UnitAura = UnitAura
-local IconLine = Cork.IconLine
 local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
 
 local spellname, _, icon = GetSpellInfo(19028)
+local IconLine = Cork.IconLine(icon, spellname)
 
 local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork "..spellname, {type = "cork", tiplink = GetSpellLink(spellname)})
 
@@ -16,14 +16,14 @@ function dataobj:Init() Cork.defaultspc[spellname.."-enabled"] = GetSpellInfo(sp
 
 local function Test(unit)
 	if Cork.dbpc[spellname.."-enabled"] and UnitExists("pet") and not UnitIsDead("pet") and not UnitAura("pet", spellname) and UnitName("pet") ~= UNKNOWN and not IsMounted() then
-		return IconLine(icon, UnitName("pet"))
+		return IconLine
 	end
 end
 
 ae.RegisterEvent("Cork "..spellname, "UNIT_PET", function(event, unit) if unit == "player" then dataobj.pet = Test() end end)
 ae.RegisterEvent("Cork "..spellname, "UNIT_AURA", function(event, unit) if unit == "pet" then dataobj.pet = Test() end end)
 
-function dataobj:Scan() self.player = Test() end
+function dataobj:Scan() self.pet = Test() end
 
 function dataobj:CorkIt(frame)
 	if self.pet then return frame:SetManyAttributes("type1", "spell", "spell", spellname) end
