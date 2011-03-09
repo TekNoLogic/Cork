1dca80701be8b76cf5339edae7f22f75f769ac27
tekkub <tekkub@gmail.com>
2011-03-09 02:14:40 -0700

    Bleepin bllop

--- modules/RogueWeapon.lua
+++ modules/RogueWeapon.lua
@@ -7,7 +7,7 @@ if Cork.MYCLASS ~= "ROGUE" then return end
 local spellids, poisonranklist = {3408, 2823, 8679, 5761, 13219}, {
 	["Crippling Poison"] = 3775,
 	["Deadly Poison"] = 2892,
-	["Instant Poison"] = { 6947,
+	["Instant Poison"] = 6947,
 	["Mind-numbing Poison"] = 5237,
 	["Wound Poison"] = 10918,
 }
