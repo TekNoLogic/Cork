0de7d1df349e6ca340f75ad501869f3f862dc34a
tekkub :bear:  <tekkub@gmail.com>
2011-05-22 20:11:49 -0600

    Don't do fancy shit for flask of enh... it doesn't work on initial login

--- modules/BuffItems.lua
+++ modules/BuffItems.lua
@@ -15,14 +15,12 @@ Cork:GenerateItemBuffer("PRIEST", UnitLevel("player") < 85 and 49632 or 62251, 6
 
 
 -- Only available to alchys
-if GetSpellInfo((GetSpellInfo(2259))) then
-	local itemname = GetItemInfo(58149) or "Flask of Enhancement"
-	local dataobj = Cork:GenerateSelfBuffer(itemname, GetItemIcon(58149), GetSpellInfo(79638), GetSpellInfo(79639), (GetSpellInfo(79640)))
-	dataobj.tiplink = "item:58149"
+local itemname = GetItemInfo(58149) or "Flask of Enhancement"
+local dataobj = Cork:GenerateSelfBuffer(itemname, GetItemIcon(58149), GetSpellInfo(79638), GetSpellInfo(79639), (GetSpellInfo(79640)))
+dataobj.tiplink = "item:58149"
 
-	function dataobj:Init() Cork.defaultspc[itemname.."-enabled"] = GetItemCount(58149) > 0 end
+function dataobj:Init() Cork.defaultspc[itemname.."-enabled"] = GetItemCount(58149) > 0 end
 
-	function dataobj:CorkIt(frame)
-		if self.player then return frame:SetManyAttributes("type1", "item", "item1", "item:58149") end
-	end
+function dataobj:CorkIt(frame)
+	if self.player then return frame:SetManyAttributes("type1", "item", "item1", "item:58149") end
 end
