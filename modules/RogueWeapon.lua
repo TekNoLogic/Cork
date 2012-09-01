f53e673f0b3745ca48bc9f40500aed322c119306
Craig R. Hughes <craig.git@rungie.com>
2012-08-31 22:16:11 -0700

    Rogue poisons are now just a straight-up buff

--- modules/RogueWeapon.lua
+++ modules/RogueWeapon.lua
@@ -3,14 +3,4 @@ local myname, Cork = ...
 if Cork.MYCLASS ~= "ROGUE" then return end
 
 
-
-local spellids, poisonranklist = {3408, 2823, 8679, 5761, 13219}, {
-	["Crippling Poison"] = 3775,
-	["Deadly Poison"] = 2892,
-	["Instant Poison"] = 6947,
-	["Mind-numbing Poison"] = 5237,
-	["Wound Poison"] = 10918,
-}
-Cork:GenerateTempEnchant(INVTYPE_WEAPONMAINHAND, spellids, 10, poisonranklist)
-Cork:GenerateTempEnchant(INVTYPE_WEAPONOFFHAND, spellids, 10, poisonranklist)
-Cork:GenerateTempEnchant(INVTYPE_THROWN, spellids, 10, poisonranklist)
+Cork:GenerateAdvancedSelfBuffer("Poison", {3408,2823,5761,8679,108211,108215})
