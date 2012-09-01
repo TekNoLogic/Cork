6ac3bbe20e50346e72dc4d0e4832ebf999408e61
Craig R. Hughes <craig.git@rungie.com>
2012-08-31 22:17:43 -0700

    Warlocks: Can't make soul shards anymore (and only applies to Affliction anyway); Remove DI; Remove armors; Add Grimoire of Sacrifice

--- modules/Warlock.lua
+++ modules/Warlock.lua
@@ -2,25 +2,6 @@
 local myname, Cork = ...
 if Cork.MYCLASS ~= "WARLOCK" then return end
 
-
--- Demon Armor
-local spellname = GetSpellInfo(687)
-Cork:GenerateAdvancedSelfBuffer(spellname, {687, 28176})
-
-
--- Dark Intent
-local spellname, _, icon = GetSpellInfo(80398)
-Cork:GenerateLastBuffedBuffer(spellname, icon, true)
-
-
---~ local di = GetSpellInfo(132) -- Detect Invisibility
---~ i = core:NewModule(di, buffs)
---~ i.target = "Friendly"
---~ i.spell = di
-
-
---~ local ueb = GetSpellInfo(5697) -- Unending Breath
---~ i = core:NewModule(ueb, buffs)
---~ i.target = "Friendly"
---~ i.spell = ueb
-
+-- Grimoire of Sacrifice
+local spellname, _, icon = GetSpellInfo(108503)
+Cork:GenerateSelfBuffer(spellname, icon)
