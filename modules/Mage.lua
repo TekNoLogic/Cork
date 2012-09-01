55df2d4da64c266009c2f9dd217a86bbc57cbc30
Craig R. Hughes <craig.git@rungie.com>
2012-08-31 22:13:13 -0700

    MoP killed Focus Magic

--- modules/Mage.lua
+++ modules/Mage.lua
@@ -7,11 +7,6 @@ if Cork.MYCLASS ~= "MAGE" then return end
 Cork:GenerateAdvancedSelfBuffer("Armor", {30482, 7302, 6117})
 
 
--- Fuckus Magic
-local spellname, _, icon = GetSpellInfo(54646)
-Cork:GenerateLastBuffedBuffer(spellname, icon)
-
-
--- Arcane Briliance
+-- Arcane Brilliance
 local altspellname, spellname, _, icon = GetSpellInfo(61316), GetSpellInfo(1459)
 Cork:GenerateRaidBuffer(spellname, icon, altspellname, true)
