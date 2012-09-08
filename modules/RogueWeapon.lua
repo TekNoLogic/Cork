a5bdc6465231b2406407eaa34760abd77a0c1618
tekkub :bear:  <tekkub@gmail.com>
2012-09-08 16:22:53 -0600

    OCD cleanup

--- modules/RogueWeapon.lua
+++ modules/RogueWeapon.lua
@@ -3,7 +3,7 @@ local myname, Cork = ...
 if Cork.MYCLASS ~= "ROGUE" then return end
 
 -- Damage poisons
-Cork:GenerateAdvancedSelfBuffer("Poison #1: Damage", {2823,8679})
+Cork:GenerateAdvancedSelfBuffer("Poison - Damage", {2823,8679})
 
 -- Utility poisons
-Cork:GenerateAdvancedSelfBuffer("Poison #2: Utility", {3408,5761,108211,108215})
+Cork:GenerateAdvancedSelfBuffer("Poison - Utility", {3408,5761,108211,108215})
