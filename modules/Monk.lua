d3e3dda8f3bd93e7ce689f8c09732a709948b3a8
James FitzGibbon <james@nadt.net>
2012-10-04 20:52:00 -0700

    Add Legacy of the Emperor support for Monks

--- modules/Monk.lua
+++ modules/Monk.lua
@@ -5,3 +5,10 @@ if Cork.MYCLASS ~= "MONK" then return end
 
 -- Stance
 Cork:GenerateAdvancedSelfBuffer("Stance", {103985, 115069, 115070}, false, true)
+
+-- Legacy of the Emperor
+local spellname, _, icon = GetSpellInfo(115921)
+local KINGS, MARK = GetSpellInfo(20217), GetSpellInfo(1126)
+Cork:GenerateRaidBuffer(spellname, icon, nil, nil, function(unit)
+	return UnitAura(unit, KINGS) or UnitAura(unit, MARK)
+end)
