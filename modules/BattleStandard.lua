44144fe5384f257ad4a176301fe7f717b349544a
tekkub :bear:  <tekkub@gmail.com>
2012-10-14 17:43:56 -0600

    Don't prompt for guild battle standard when it's on CD

--- modules/BattleStandard.lua
+++ modules/BattleStandard.lua
@@ -1,15 +1,22 @@
 
 local myname, Cork = ...
+local UnitAura = Cork.UnitAura or UnitAura
+local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
 
 local ally = UnitFactionGroup('player') == "Alliance"
 local ids = ally and {64399, 64398, 63359} or {64402, 64401, 64400}
 local buffname = GetSpellInfo(90633)
+local name = "Cork "..buffname
+local iconline = Cork.IconLine(GetItemIcon(ids[1]), buffname)
+local coolingdown
 
 
-local dataobj = Cork:GenerateSelfBuffer(buffname, GetItemIcon(ids[1]))
-dataobj.tiplink = "item:"..ids[1]
-dataobj.lowpriority = true
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject(name, {
+	type = "cork", tiplink = GetSpellLink(buffname),
+	tiplink = "item:"..ids[1],
+	lowpriority = true,
+})
 
 
 local function FindItem()
@@ -19,9 +26,39 @@ local function FindItem()
 end
 
 
+local function HasBuff()
+	return UnitAura("player", buffname)
+end
+
+
+local function CooldownFinish()
+	coolingdown = false
+	dataobj:Scan()
+end
+
+
 function dataobj:Init()
 	Cork.defaultspc[buffname.."-enabled"] = FindItem() ~= nil
 end
+
+
+local function Test(unit)
+	if coolingdown or not Cork.dbpc[buffname.."-enabled"] or HasBuff() then return end
+
+	local id = FindItem()
+	if not id then return end
+
+	local start, duration = GetItemCooldown(id)
+	if start > 0 then
+		coolingdown = true
+		Cork.StartTimer(start + duration, CooldownFinish)
+	elseif not (IsResting() and not Cork.db.debug) then
+		return iconline
+	end
+end
+
+
+function dataobj:Scan() self.player = Test() end
 
 
 function dataobj:CorkIt(frame)
@@ -30,3 +67,9 @@ function dataobj:CorkIt(frame)
 		return frame:SetManyAttributes("type1", "item", "item1", "item:"..itemowned)
 	end
 end
+
+
+ae.RegisterEvent(dataobj, "PLAYER_UPDATE_RESTING", "Scan")
+ae.RegisterEvent(name, "UNIT_AURA", function(event, unit)
+	if unit == "player" then dataobj.player = Test() end
+end)
