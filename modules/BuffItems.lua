49a4857ec371a31d1d61622279ed42b7c70e6d7d
tekkub :bear:  <tekkub@gmail.com>
2012-11-03 02:09:10 -0600

    Add new fortitude runescroll

--- modules/BuffItems.lua
+++ modules/BuffItems.lua
@@ -1,9 +1,10 @@
 
 local myname, Cork = ...
+local level = UnitLevel("player")
 
 
 -- Items only available at 80
-if UnitLevel("player") < 80 then return end
+if level < 80 then return end
 
 
 -- Drums of Forgotten Kings
@@ -11,7 +12,8 @@ Cork:GenerateItemBuffer({PALADIN = true, DRUID = true}, 49633, 69378, 20217)
 
 
 -- Runescroll of Fortitude
-Cork:GenerateItemBuffer("PRIEST", UnitLevel("player") < 85 and 49632 or 62251, 69377, 21562)
+local id = level < 85 and 49632 or level < 90 and 62251 or 79257
+Cork:GenerateItemBuffer("PRIEST", id, 69377, 21562)
 
 
 -- Only available to alchys
