14f9f32f8d5bab47be920d3601f50023a98ba03b
Kevin Ballard <kevin@sb.org>
2013-02-01 16:16:59 -0800

    Stop using #showtooltip just for icon purposes
    In the Macro generator, #showtooltip isn't very useful in macros that
    don't have an in-combat action. Just set the macro icon directly in
    those cases.
    
    This has the side-effect of showing the desired icon even when the
    player doesn't have the spell yet (due to being low level).

--- MacroGenerator.lua
+++ MacroGenerator.lua
@@ -13,25 +13,35 @@ function Cork.GenerateMacro()
 	if id then PickupMacro(id)
 	elseif select(2, GetNumMacros()) < MAX_CHARACTER_MACROS then
 		local body, ic, ooc
+		local icon = "INV_MISC_QUESTIONMARK"
 		local c = Cork.MYCLASS
-		if c == "DEATHKNIGHT" then ooc = GetSpellInfo(3714)
-		elseif c == "HUNTER"  then ooc = GetSpellInfo(13165)
-		elseif c == "SHAMAN"  then ooc = GetSpellInfo(324)
-		elseif c == "WARLOCK" then ooc = GetSpellInfo(6201)
-		elseif c == "WARRIOR" then ooc = GetSpellInfo(6673)
-		elseif c == "MONK"    then ooc = GetSpellInfo(115921)
-		elseif c == "DRUID"   then ic, ooc = GetSpellInfo(22812), GetSpellInfo(1126)
-		elseif c == "MAGE"    then ic, ooc = GetSpellInfo(30482), GetSpellInfo(1459)
-		elseif c == "PALADIN" then ic, ooc = GetSpellInfo(21084), GetSpellInfo(19740)
-		elseif c == "PRIEST"  then ic, ooc = GetSpellInfo(588),   GetSpellInfo(21562) end
+		if c == "DEATHKNIGHT" then ooc = 3714
+		elseif c == "HUNTER"  then ooc = 13165
+		elseif c == "SHAMAN"  then ooc = 324
+		elseif c == "WARLOCK" then ooc = 6201
+		elseif c == "WARRIOR" then ooc = 6673
+		elseif c == "MONK"    then ooc = 115921
+		elseif c == "DRUID"   then ic, ooc = 22812, 1126
+		elseif c == "MAGE"    then ic, ooc = 30482, 1459
+		elseif c == "PALADIN" then ic, ooc = 21084, 19740
+		elseif c == "PRIEST"  then ic, ooc = 588,   21562
+		end
 		if ic and ooc then
+			ic, ooc = GetSpellInfo(ic), GetSpellInfo(ooc)
 			body = "#showtooltip [combat] "..ic.."; "..ooc.."\n/cast [combat] "..ic.."\n/stopmacro [combat]\n/click CorkFrame"
 		elseif ooc then
-			body = "#showtooltip "..ooc.."\n/click CorkFrame"
+			local ooc, _, texture = GetSpellInfo(ooc)
+			texture = texture:upper():match("^INTERFACE\\ICONS\\(.+)")
+			if texture then
+				icon = texture
+				body = "/click CorkFrame"
+			else
+				body = "#showtooltip "..ooc.."\n/click CorkFrame"
+			end
 		else
 			body = "/click CorkFrame"
 		end
-		local id = CreateMacro("Cork", "INV_MISC_QUESTIONMARK", body, true)
+		local id = CreateMacro("Cork", icon, body, true)
 		PickupMacro(id)
 	end
 end
