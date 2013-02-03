59c4d58b770a846d6165d852d082f61bcce718dd
tekkub :bear:  <tekkub@gmail.com>
2013-02-03 16:16:33 -0700

    80col

--- modules/Druid.lua
+++ modules/Druid.lua
@@ -42,5 +42,7 @@ local dobj, ref = Cork:GenerateAdvancedSelfBuffer("Fursuit", {768, 5487, 24858})
 function dobj:CorkIt(frame)
 	ref()
 	local spell = Cork.dbpc["Fursuit-spell"]
-	if self.player and Corkboard:NumLines() == 1 then return frame:SetManyAttributes("type1", "spell", "spell", spell, "unit", "player") end
+	if self.player and Corkboard:NumLines() == 1 then
+		return frame:SetManyAttributes("type1", "spell", "spell", spell, "unit", "player")
+	end
 end
