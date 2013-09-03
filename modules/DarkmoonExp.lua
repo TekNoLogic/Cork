a48b2fd929913cf8f614852252d4974a69dc00bb
tekkub :bear:  <tekkub@gmail.com>
2013-09-03 02:00:36 -0600

    Better handling of maxlevel in darkmoon module

--- modules/DarkmoonExp.lua
+++ modules/DarkmoonExp.lua
@@ -16,14 +16,15 @@ local function DarkmoonToday()
 end
 
 
+local maxlevel = GetMaxPlayerLevel()
 local itemname = "Darkmoon EXP Buff"
 local spellname, _, icon = GetSpellInfo(46668)
 local hatspell = GetSpellInfo(136583)
 local dataobj = Cork:GenerateSelfBuffer(itemname, icon, spellname, hatspell)
 function dataobj:Test() return DarkmoonToday() and self:TestWithoutResting() end
 function dataobj:Init()
-	Cork.defaultspc[itemname.."-enabled"] = level < 90
-	if level < 90 then OpenCalendar() end
+	Cork.defaultspc[itemname.."-enabled"] = level < maxlevel
+	if level < maxlevel then OpenCalendar() end
 end
 ae.RegisterEvent(dataobj, "CALENDAR_UPDATE_EVENT_LIST", "Scan")
 dataobj.tiplink = "spell:46668"
