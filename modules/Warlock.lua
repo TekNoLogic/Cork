366070066b7dfe5dbb70e57cbb017f5aae7e5dd6
Adirelle <adirelle@gmail.com>
2013-11-07 23:57:19 +0100

    Warlocks: check for alternatives of Dark Intent.

--- modules/Warlock.lua
+++ modules/Warlock.lua
@@ -4,8 +4,17 @@ if Cork.MYCLASS ~= "WARLOCK" then return end
 
 
 -- Dark Intent
+local FORT, CSHOUT = GetSpellInfo(21562), GetSpellInfo(469)
+local ARCB, BWRATH = GetSpellInfo(1459), GetSpellInfo(77747)
 local spellname, _, icon = GetSpellInfo(109773)
-Cork:GenerateRaidBuffer(spellname, icon)
+Cork:GenerateRaidBuffer(spellname, icon, nil, nil, function(unit)
+	if UnitAura(unit, spellname) then return true end
+	if (UnitAura(unit, FORT) or UnitAura(unit, CSHOUT)) -- Another stamina buff
+		and (UnitAura(unit, ARCB) or UnitAura(unit, BWRATH) or UnitPowerType(unit) ~= SPELL_POWER_MANA) -- Another spellpower buff (or not a mana user)
+	then
+		return true
+	end
+end)
 
 
 -- Soulstone
