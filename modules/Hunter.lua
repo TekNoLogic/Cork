4e76e53f84b237649419e2fe11108e3d54418b73
tekkub :bear:  <tekkub@gmail.com>
2014-10-24 22:48:59 -0600

    Hunter updates for 6.0

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -4,4 +4,4 @@ if Cork.MYCLASS ~= "HUNTER" then return end
 
 
 -- Aspects
-Cork:GenerateAdvancedSelfBuffer("Aspects", {13165, 5118, 13159, 61648, 109260})
+Cork:GenerateAdvancedSelfBuffer("Aspects", {5118, 13159, 61648})
