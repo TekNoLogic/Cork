7847c23377e5e73708ddf609a216c4a3142318eb
tekkub :bear:  <tekkub@gmail.com>
2014-10-24 22:41:27 -0600

    Mage fixes for 6.0

--- modules/Mage.lua
+++ modules/Mage.lua
@@ -3,10 +3,6 @@ local myname, Cork = ...
 if Cork.MYCLASS ~= "MAGE" then return end
 
 
--- Armor
-Cork:GenerateAdvancedSelfBuffer("Armor", {30482, 7302, 6117})
-
-
 -- Arcane Brilliance
 local altspellname, spellname, _, icon = GetSpellInfo(61316), GetSpellInfo(1459)
 Cork:GenerateRaidBuffer(spellname, icon, altspellname, true)
