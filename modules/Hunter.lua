ca488c1b9f633285c401cdd04fe05a0695ae2db3
Tarkumi <tarkumi@gmail.com>
2014-10-27 16:41:05 +0100

    Hunter.lua suggested changes
    Trap Launcher and Exotic Munitions support.

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -5,3 +5,12 @@ if Cork.MYCLASS ~= "HUNTER" then return end
 
 -- Aspects
 Cork:GenerateAdvancedSelfBuffer("Aspects", {5118, 13159, 61648})
+
+-- Exotic Munitions
+local _, _, _, selected, _ = GetTalentInfo(7,1,GetActiveSpecGroup())
+if selected then
+	Cork:GenerateAdvancedSelfBuffer("Exotic Munitions", {162536, 162537, 162539})
+end
+
+--	Trap launcher
+Cork:GenerateAdvancedSelfBuffer("Trap Launcher", {77769})
