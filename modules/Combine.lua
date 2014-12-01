805a38ace53c896347f175f7ff5ed35522b19cb0
tekkub :bear:  <tekkub@gmail.com>
2014-11-30 22:14:31 -0700

    Clean up combine list

--- modules/Combine.lua
+++ modules/Combine.lua
@@ -4,12 +4,6 @@ local IconLine = Cork.IconLine
 local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
 local ITEMS = {
-  [37700] = 10,
-  [37701] = 10,
-  [37702] = 10,
-  [37703] = 10,
-  [37704] = 10,
-  [37705] = 10,
   [33567] = 5,
   [34056] = 3,
   [52718] = 3,
@@ -17,51 +11,30 @@ local ITEMS = {
   [52977] = 5,
   [89112] = 10,
   [90407] = 10,
-  [115504] = 10,
   [115502] = 10,
-  [109624] = 10,
-  [109625] = 10,
-  [109626] = 10,
-  [109628] = 10,
-  [109629] = 10,
+  [115504] = 10,
+
   [110610] = 10,
   [108391] = 10,
   [109991] = 10,
   [109992] = 10,
-  -- Draenor fish: small
+
   [111589] = 20,
-  [111650] = 20,
-  [111651] = 20,
-  [111652] = 20,
-  [111656] = 20,
-  [111658] = 20,
-  [111659] = 20,
   [111662] = 20,
   [118564] = 20,
-  -- normal size
   [111595] = 10,
-  [111663] = 10,
-  [111664] = 10,
-  [111665] = 10,
-  [111666] = 10,
-  [111667] = 10,
-  [111668] = 10,
-  [111669] = 10,
-  [118565] = 10,
-  -- enormous
+	[118565] = 10,
   [111601] = 5,
-  [111670] = 5,
-  [111671] = 5,
-  [111672] = 5,
-  [111673] = 5,
-  [111674] = 5,
-  [111675] = 5,
-  [111676] = 5,
   [118566] = 5,
 }
 
+for i=37700,37705 do ITEMS[i] = 10 end   -- crystallized elements
 for i=108318,108365 do ITEMS[i] = 10 end -- herbalism
+for i=109624,109629 do ITEMS[i] = 10 end -- draenor herbalism
 for i=108294,108309 do ITEMS[i] = 10 end -- mining
+for i=111650,111659 do ITEMS[i] = 20 end -- small draenor fish
+for i=111663,111669 do ITEMS[i] = 10 end -- medium draenor fish
+for i=111670,111676 do ITEMS[i] = 5 end  -- large draenor fish
 
 
 Cork.defaultspc["Combine-enabled"] = true
