b2e39a9cd8350b021fa9df5b50170a91ef0f19ad
Adirelle <adirelle@gmail.com>
2014-12-12 21:54:16 +0100

    Add hunters' Lone Wolf buffs.

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -9,3 +9,6 @@ Cork:GenerateAdvancedSelfBuffer("Aspects", {5118, 13159, 61648})
 -- Trap Launcher
 local spellname, _, icon = GetSpellInfo(77769)
 Cork:GenerateSelfBuffer(spellname, icon)
+
+-- Lone Wolf buffs
+Cork:GenerateAdvancedSelfBuffer("Lone Wolf", {160198, 160199, 160200, 160203, 160205, 160206, 172967, 172968})
