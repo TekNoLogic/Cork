686a190b117b57723ee56b2a51a9b009a69f2b1a
Adirelle <adirelle@gmail.com>
2014-12-12 21:53:29 +0100

    Add hunters' Trap Launcher.

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -5,3 +5,7 @@ if Cork.MYCLASS ~= "HUNTER" then return end
 
 -- Aspects
 Cork:GenerateAdvancedSelfBuffer("Aspects", {5118, 13159, 61648})
+
+-- Trap Launcher
+local spellname, _, icon = GetSpellInfo(77769)
+Cork:GenerateSelfBuffer(spellname, icon)
