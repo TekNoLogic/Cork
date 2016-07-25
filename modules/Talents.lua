7f159b3db6c9847321f398cff258c946df1922ae
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-07-24 18:49:03 -0600

    Hide the default "unspent talent points" hint

--- modules/Talents.lua
+++ modules/Talents.lua
@@ -4,18 +4,31 @@ local SpellCastableOnUnit = Cork.SpellCastableOnUnit
 local ldb, ae = LibStub:GetLibrary("LibDataBroker-1.1"), LibStub("AceEvent-3.0")
 
 local IconLine = Cork.IconLine("Interface\\Icons\\Ability_Marksmanship", "Unspent talent points")
-local defaults = Cork.defaultspc
-defaults["Talents-enabled"] = true
+Cork.defaultspc["Talents-enabled"] = true
 
-local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Cork Talents", {type = "cork", tiptext = "Warn when you have unspent talent points."})
+local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject(
+  "Cork Talents",
+  {type = "cork", tiptext = "Warn when you have unspent talent points."}
+)
+
+function dataobj:Init()
+  if Cork.dbpc["Talents-enabled"] then TalentMicroButtonAlert:Hide() end
+end
 
 local function talentlesshack()
  	return GetNumUnspentTalents() > 0
 end
-local function Test() return Cork.dbpc["Talents-enabled"] and talentlesshack() and IconLine end
+local function Test()
+  return Cork.dbpc["Talents-enabled"] and talentlesshack() and IconLine
+end
 
 function dataobj:Scan() dataobj.player = Test() end
 
 ae.RegisterEvent("Cork Talents", "CHARACTER_POINTS_CHANGED", dataobj.Scan)
 ae.RegisterEvent("Cork Talents", "PLAYER_TALENT_UPDATE", dataobj.Scan)
 ae.RegisterEvent("Cork Talents", "ACTIVE_TALENT_GROUP_CHANGED", dataobj.Scan)
+
+
+TalentMicroButtonAlert:SetScript("OnShow", function(self)
+  if Cork.dbpc["Talents-enabled"] then self:Hide() end
+end)
