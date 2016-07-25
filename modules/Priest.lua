afcbf5f9ddda52c04ac14e2ebdd11cf395999370
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-07-24 18:38:22 -0600

    Looks like all the priest buffs are gone now

--- modules/Priest.lua
+++ modules/Priest.lua
@@ -1,25 +1,3 @@
 
 local myname, Cork = ...
 if Cork.MYCLASS ~= "PRIEST" then return end
-
-
--- Fort
-local DARKINT, spellname, _, icon = GetSpellInfo(109773), GetSpellInfo(21562)
-Cork:GenerateRaidBuffer(spellname, icon, nil, nil, function(unit)
-	if UnitAura(unit, DARKINT) then return true end
-end)
-
-
--- Shadowform
-local spellname, _, icon = GetSpellInfo(15473)
-Cork:GenerateSelfBuffer(spellname, icon)
-
-
--- Fear Ward
-local spellname, _, icon = GetSpellInfo(6346)
-local dataobj = Cork:GenerateLastBuffedBuffer(spellname, icon)
-dataobj.onlyrebuffs = true
-
-
--- Chakras
-Cork:GenerateAdvancedSelfBuffer("Chakra", {81206, 81208, 81209})
