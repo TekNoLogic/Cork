e29b0854872b069a8187a4ec6d1537e24b827148
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-07-26 04:53:53 -0600

    Quick simple fix for pally blessings

--- modules/Paladin.lua
+++ modules/Paladin.lua
@@ -12,6 +12,21 @@ local spellname, _, icon = GetSpellInfo(25780)
 Cork:GenerateSelfBuffer(spellname, icon)
 
 
+-- Greater Blessing of Might
+local spellname, _, icon = GetSpellInfo(203528)
+Cork:GenerateSelfBuffer(spellname, icon)
+
+
+-- Greater Blessing of Kings
+local spellname, _, icon = GetSpellInfo(203538)
+Cork:GenerateSelfBuffer(spellname, icon)
+
+
+-- Greater Blessing of Wisdom
+local spellname, _, icon = GetSpellInfo(203539)
+Cork:GenerateSelfBuffer(spellname, icon)
+
+
 -- Beacon of Light
 local spellname, _, icon = GetSpellInfo(53563)
 local dataobj = Cork:GenerateLastBuffedBuffer(spellname, icon)
