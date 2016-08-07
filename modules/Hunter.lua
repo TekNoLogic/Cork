c46a0ac0339048ef5b26bff5e826d5e2ca0e01ef
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-08-07 02:08:57 -0600

    Dead hunter buffs

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -1,12 +1,3 @@
 
 local myname, Cork = ...
 if Cork.MYCLASS ~= "HUNTER" then return end
-
-
--- Exotic Munitions
-Cork:GenerateAdvancedSelfBuffer("Exotic Munitions", {162537, 162536, 162539})
-
-
--- Trap Launcher
-local spellname, _, icon = GetSpellInfo(77769)
-Cork:GenerateSelfBuffer(spellname, icon)
