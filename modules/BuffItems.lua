a5c270d382aa3d860ac81a9d6850f5c4a3ac23ad
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-08-07 02:08:45 -0600

    Only enable minifier if we have a bodyguard

--- modules/BuffItems.lua
+++ modules/BuffItems.lua
@@ -47,3 +47,21 @@ end
 -- Bodyguard Miniaturization Device
 local dataobj = Cork:GenerateItemSelfBuffer(122298)
 dataobj.Test = dataobj.TestWithoutResting
+
+local function HasBodyguard()
+	local buildings = C_Garrison.GetBuildings(2)
+	for i,building in pairs(buildings) do
+		if building.buildingID == 27 or building.buildingID == 28 then
+			return not not C_Garrison.GetFollowerInfoForBuilding(building.plotID)
+		end
+	end
+end
+
+local orig = dataobj.Init
+function dataobj:Init()
+	orig(self)
+	if Cork.defaultspc[self.name.."-enabled"] then
+		Cork.defaultspc[self.name.."-enabled"] = HasBodyguard()
+	end
+	self.Init = nil
+end
