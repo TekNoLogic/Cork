94d0ff5bcd5c2bf654fd593e2d4c57cf2708df0d
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-09-06 22:06:44 -0600

    Buhbye arcint

--- modules/Mage.lua
+++ modules/Mage.lua
@@ -3,11 +3,6 @@ local myname, Cork = ...
 if Cork.MYCLASS ~= "MAGE" then return end
 
 
--- Arcane Brilliance
-local altspellname, spellname, _, icon = GetSpellInfo(61316), GetSpellInfo(1459)
-Cork:GenerateRaidBuffer(spellname, icon, altspellname, true)
-
-
 -- Ice Barrier
 local spellname, _, icon = GetSpellInfo(11426)
 local dataobj = Cork:GenerateSelfBuffer(spellname, icon)
