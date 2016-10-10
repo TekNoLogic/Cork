3b5a0143993f0523c542324fd7a71ec4f776d17f
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-09 18:55:31 -0600

    So long MotW, you will be missed

--- modules/Druid.lua
+++ modules/Druid.lua
@@ -3,22 +3,6 @@ local myname, Cork = ...
 if Cork.MYCLASS ~= "DRUID" then return end
 
 
--- Mark of the Wild
-local spellname, _, icon = GetSpellInfo(1126)
-local EMP, KINGS = GetSpellInfo(115921), GetSpellInfo(20217)
-local GRACE, MIGHT = GetSpellInfo(116956), GetSpellInfo(19740)
-Cork:GenerateRaidBuffer(spellname, icon, nil, nil, function(unit)
-	-- If a monk already hit this unit, we don't need to
-	if UnitAura(unit, EMP) then return true end
-
-	-- If a pally cast Kings when he should have put up Might, overwrite it
-	if UnitAura(unit, KINGS) then
-		-- If either mastery buff is also present, we're good and don't need to buff
-		return UnitAura(unit, MIGHT) or UnitAura(unit, GRACE)
-	end
-end)
-
-
 -- Shapeshifts
 local dobj, ref = Cork:GenerateAdvancedSelfBuffer("Fursuit", {768, 5487, 24858})
 function dobj:CorkIt(frame)
